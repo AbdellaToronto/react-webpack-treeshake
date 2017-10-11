a3e15dd3bc87ab02580f0899e3289235941b47f4
Abdella <abdella@rangle.io>
2017-10-11 09:13:20 -0400

    include minification and tree shaking

---
+++ client/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const App = () =>
+  (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Hello World</h1>
+    </div>
+  );
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,45 @@
 const path = require('path');
+const webpack = require('webpack');
+const BabelMinifyPlugin = require('babel-minify-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './client/index.html',
   filename: 'index.html',
   inject: 'body'
-})
+});
 
 module.exports = {
   entry: './client/index.js',
   output: {
     path: path.resolve('dist'),
-    // below line only works for webpack 1.0
-    // path: './dist', 
-    filename: 'index_bundle.js'
+    filename: '[name].bundle.js'
   },
   module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      include: [
+        path.resolve(__dirname, 'client')
+      ]
+    }, {
+      test: /\.json$/,
+      loader: "json-loader"
+    }]
   },
-  plugins: [HtmlWebpackPluginConfig]
-}
+  plugins: [
+    HtmlWebpackPluginConfig,
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
+    }),
+    new BabelMinifyPlugin()
+  ],
+  resolve: {
+    modules: [
+      path.join(process.cwd(), 'app'),
+      'node_modules'
+    ],
+    extensions: ['.js', '.json']
+  },
+  devtool: false
+};
+;
