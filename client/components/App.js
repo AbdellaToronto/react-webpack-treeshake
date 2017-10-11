ad1fec1fb94cc4f1bb87f0adabf780f92629ddba
Abdella <abdella@rangle.io>
2017-10-11 16:16:01 -0400

    treeshaking webpack base

--- client/components/App.js
+++ client/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const App = () =>
   (
     <div style={{ textAlign: 'center' }}>
-      <h1>Hello World</h1>
+      <h1>Hello eTrade</h1>
     </div>
   );
 
--- webpack.config.js
+++ webpack.config.js
@@ -42,4 +42,4 @@ module.exports = {
   },
   devtool: false
 };
-;
+
